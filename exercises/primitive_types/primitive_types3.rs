e51d7c273bf24c13068da7220b97e960cf474ce9
Walvka <2411082614@qq.com>
2023-08-07 22:49:04 +0800

    [1]继续做题

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,11 +5,9 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
-
+    let a = [0; 107];
+    let a:[i32; 107] = [0; 107];
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cost: i32) -> i32 {
+    if cost <= 40 {
+        2 * cost
+    }else{
+        cost
+    }
+}
 
 // Don't modify this function!
 #[test]
